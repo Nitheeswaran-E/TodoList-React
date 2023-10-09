25fe6493a811de7cec6688d375a44b4a3c418bb7
Nitheeswaran <114794641+Nitheeswaran-E@users.noreply.github.com>
2023-10-09 22:53:15 +0530

    Add files via upload

--- src/TodoList/Todo.jsx
+++ src/TodoList/Todo.jsx
@@ -6,7 +6,8 @@ const Todo = (props) => {
             border: '1px solid black',
             display: 'flex',
             justifyContent: 'space-evenly',
-            alignItems:'center'
+            alignItems:'center',
+            fontSize: '10px'
         },
         completedStyle: {
             textDecoration:  props.isCompleted && 'line-through'
@@ -27,8 +28,21 @@ const Todo = (props) => {
         <button style={styles.btns} onClick={() => { props.completeTask(props.sno)}}> {props.isCompleted?'Not Completed':'Completed'}</button>
       </div>
       <div style={styles.taskInfoDiv}>
-        <h1 style={ styles.completedStyle }>{props.name}</h1>
-        <h3 style={ styles.completedStyle }>{props.desc}</h3>
+      <table>
+          <tr>
+            <td>
+            <h1 style={ styles.completedStyle }>Task Name:</h1>
+            </td>
+            <td>
+             <h1 style={ styles.completedStyle }> {props.name}</h1>
+            </td>
+          </tr>
+        </table>
+        <h1 style={ styles.completedStyle }>Task Name: {props.name}</h1>
+        <h3 style={ styles.completedStyle }>Description: {props.desc}</h3>
+        <h3 style={styles.completedStyle }>Tags: {props.tag}</h3>
+  
+        <p  style={styles.completedStyle}>Due date: {props.dueDate}</p>
       </div>
       <div>
         <button style={styles.btns} onClick={() => { props.missTask(props.sno)}}> {props.isMissed?'Not Missed':'Missed'}</button>
--- src/TodoList/todoList.jsx
+++ src/TodoList/todoList.jsx
@@ -1,66 +1,91 @@
 import React, { useEffect, useState } from "react";
 import Todo from "./Todo";
 
+class TodoMemento {
+  constructor(state) {
+    this.state = state;
+  }
+  getState() {
+    return this.state;
+  }
+}
+class TodoCaretaker {
+  constructor() {
+    this.mementos = [];
+    this.currentIndex = -1;
+  }
+  getMemento() {
+    return this.mementos[this.mementos.length - 1]
+  }
+  addMemento(memento) {
+    this.mementos.push(memento);
+    this.currentIndex = this.mementos.length - 1;
+  }
+  undo() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      return this.mementos[this.currentIndex];
+    }
+    return null;
+  }
+  redo() {
+    if (this.currentIndex < this.mementos.length - 1) {
+      this.currentIndex++;
+      return this.mementos[this.currentIndex];
+    }
+    return null;
+  }
+}
 const TodoList = () => {
-  const [caretaker] = useState(new TodoCaretaker()); 
+  const [caretaker] = useState(new TodoCaretaker());
   const [completed, setCompleted] = useState(3);
   const [missed, setMissed] = useState(1);
   const [sno, setSno] = useState(4)
   const [taskName, setTaskName] = useState('')
   const [taskDesc, setTaskDesc] = useState('')
+  const [dueDate, setDate] = useState(null)
   const [totalTasks, setTotalTasks] = useState(0)
+  const [tag, setTag] = useState('')
   const [todos, setTodos] = useState(
     [
       {
         sno: 1,
         name: "Full stack Lab",
         desc: "Need to Complete ex-7,8",
-        isCompleted:false,
-        isMissed:false
+        isCompleted: false,
+        isMissed: false,
+        dueDate: new Date("2022-03-25").toISOString().split('T')[0],
+        tag: "Full Stack"
+
       },
       {
         sno: 2,
-        name: "Deep Learning Lab",
-        desc: "Need to Complete ex-4,5,6",
-        isCompleted:false,
-        isMissed:false
+        name: "Full stack Lab",
+        desc: "Need to Complete ex-7,8",
+        isCompleted: true,
+        isMissed: false,
+        dueDate: new Date("2022-03-25").toISOString().split('T')[0],
+        tag: "Full Stack"
+
       },
       {
         sno: 3,
-        name: "POSE Lab",
-        desc: "Need to Complete Reveiew-2",
-        isCompleted:false,
-        isMissed:false
-      },
-      {
-        sno: 4,
-        name: "Full stack Lab",
-        desc: "Need to Complete ex-7,8",
-        isCompleted:true,
-        isMissed:false
-      },
-      {
-        sno: 5,
         name: "Deep Learning Lab",
         desc: "Need to Complete ex-4,5,6",
-        isCompleted:false,
-        isMissed:true
-      },
-      {
-        sno: 6,
-        name: "POSE Lab",
-        desc: "Need to Complete Reveiew-2",
-        isCompleted:false,
-        isMissed:false
-      },
+        isCompleted: false,
+        isMissed: true,
+        dueDate: new Date("2022-03-25").toISOString().split('T')[0],
+        tag: "DL"
+
+      }
     ]
   )
   const [backUp,] = useState(todos)
-  useEffect(() => { 
+  useEffect(() => {
     setTotalTasks(todos.length)
     caretaker.addMemento(new TodoMemento(todos));
-  },[])
-  
+  }, [])
+
   const styles = {
     navbar: {
       display: "flex",
@@ -71,79 +96,81 @@ const TodoList = () => {
     },
     div: {
       display: 'flex',
-      gap:'20px'
+      gap: '20px'
     }
   };
-  const compeleteTask = (sno) =>{
+  const compeleteTask = (sno) => {
     const updatedTodos = todos.map((todo) =>
-        todo.sno === sno ? { ...todo, isCompleted: !todo.isCompleted } : todo
+      todo.sno === sno ? { ...todo, isCompleted: !todo.isCompleted } : todo
     );
     setTodos(updatedTodos)
     caretaker.addMemento(new TodoMemento(updatedTodos));
     console.log(caretaker.getMemento())
-    let count = todos.map(todo => todo.sno === sno ? todo.isCompleted : null )[0]
+    let count = todos.map(todo => todo.sno === sno ? todo.isCompleted : null)[0]
     console.log(count)
-    count = count?-1:1
+    count = count ? -1 : 1
     console.log(count)
     setCompleted(completed + count)
-    setTotalTasks(totalTasks-count)
-}
-const missTask = (sno) =>{
+    setTotalTasks(totalTasks - count)
+  }
+  const missTask = (sno) => {
     const updatedTodos = todos.map((todo) =>
-        todo.sno === sno ? { ...todo, isMissed: !todo.isMissed } : todo
+      todo.sno === sno ? { ...todo, isMissed: !todo.isMissed } : todo
     );
     setTodos(updatedTodos)
     caretaker.addMemento(new TodoMemento(updatedTodos));
     console.log(caretaker.getMemento())
-    let count = todos.map(todo => todo.sno === sno ? todo.isMissed : null )[0]
+    let count = todos.map(todo => todo.sno === sno ? todo.isMissed : null)[0]
     console.log(count)
-    count = count?-1:1
+    count = count ? -1 : 1
     console.log(count)
     setMissed(missed + count)
-    setTotalTasks(totalTasks-count)
+    setTotalTasks(totalTasks - count)
   }
   const addTask = () => {
-    setTodos([...todos,{
+    setTodos([...todos, {
       sno: sno,
       name: taskName,
-      desc:taskDesc
+      desc: taskDesc,
+      dueDate: dueDate,
+      tag: tag
     }])
-    caretaker.addMemento(new TodoMemento([...todos,{
-        sno: sno,
-        name: taskName,
-        desc:taskDesc
+    caretaker.addMemento(new TodoMemento([...todos, {
+      sno: sno,
+      name: taskName,
+      desc: taskDesc
     }]));
-    setSno(sno+1)
+    setSno(sno + 1)
     setTaskName('')
     setTaskDesc('')
-    setTotalTasks(totalTasks+1)
+    setTotalTasks(totalTasks + 1)
   }
   const removeTask = (sno) => {
-    setTodos(todos.filter(todo=>todo.sno!==sno))
-    caretaker.addMemento(new TodoMemento(todos.filter(todo=>todo.sno!==sno)));
+    setTodos(todos.filter(todo => todo.sno !== sno))
+    caretaker.addMemento(new TodoMemento(todos.filter(todo => todo.sno !== sno)));
   }
   const sortByCompleted = (sno) => {
-    setTodos(todos.filter(todo=>todo.isCompleted===true))
-    caretaker.addMemento(new TodoMemento(todos.filter(todo=>todo.isCompleted===true)));
+    setTodos(todos.filter(todo => todo.isCompleted === true))
+    caretaker.addMemento(new TodoMemento(todos.filter(todo => todo.isCompleted === true)));
   }
   const sortByMissed = (sno) => {
-    setTodos(todos.filter(todo=>todo.isMissed===true))
-    caretaker.addMemento(new TodoMemento(todos.filter(todo=>todo.isMissed===true)));
+    setTodos(todos.filter(todo => todo.isMissed === true))
+    caretaker.addMemento(new TodoMemento(todos.filter(todo => todo.isMissed === true)));
   }
-  const reset = () => { 
+  const reset = () => {
     setTodos(backUp)
     caretaker.addMemento(new TodoMemento(backUp));
   }
   const undo = () => {
     const memento = caretaker.undo()
-    console.log('undo',memento.getState())
+    console.log('undo', memento.getState())
     if (memento) {
-        setTodos(memento.getState());
+      setTodos(memento.getState());
     }
   }
   const redo = () => {
     const memento = caretaker.redo();
-    console.log('redo',memento.getState())
+    console.log('redo', memento.getState())
     if (memento) {
       setTodos(memento.getState());
     }
@@ -152,70 +179,41 @@ const missTask = (sno) =>{
     <div>
       <div style={styles.navbar}>
         <h2>Todo App</h2>
-        <h3>Tasks Remaining: { totalTasks }</h3>
-        <h3>Total Completed Tasks: {completed}</h3>
-        <h3> Total Missed Tasks: {missed}</h3>
+        <h5>Tasks Remaining: {totalTasks}</h5>
+        <h5>Total Completed Tasks: {completed}</h5>
+        <h5> Total Missed Tasks: {missed}</h5>
         <div style={styles.div}>
           <button style={styles.addTaskBtn} onClick={addTask}>Add Task+</button>
-          <input onChange={(e)=>setTaskName(e.target.value)} type="text" placeholder="Enter task name" />
-          <input onChange={(e)=>setTaskDesc(e.target.value)} type="textarea" placeholder="Enter task description" />
+          <input onChange={(e) => setTaskName(e.target.value)} type="text" placeholder="Enter task name" />
+          <input onChange={(e) => setTaskDesc(e.target.value)} type="textarea" placeholder="Enter task description" />
+          <input onChange={(e) => setDate(e.target.value)} type="date" placeholder="Enter due date" />
+          <input onChange={(e) => setTag(e.target.value)} type="text" placeholder="Enter a Tag" />
+
         </div>
       </div>
-      {todos.map((task) => ( 
+      <div>
+        <button onClick={sortByCompleted}>Completed</button>
+        <button onClick={sortByMissed}>Missed</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={undo}>undo</button>
+        <button onClick={redo}>redo</button>
+
+      </div>
+      {todos.map((task) => (
         <Todo
           sno={task.sno}
           name={task.name}
-          desc={task.desc} 
+          desc={task.desc}
           isMissed={task.isMissed}
           isCompleted={task.isCompleted}
           removeTask={removeTask}
           completeTask={compeleteTask}
           missTask={missTask}
+          dueDate={task.dueDate}
+          tag={task.tag}
         />
       ))}
-      <button onClick={sortByCompleted}>Completed</button>
-      <button onClick={sortByMissed}>Missed</button>
-      <button onClick={reset}>Reset</button>
-      <button onClick={undo}>undo</button>
-      <button onClick={redo}>redo</button>
     </div>
   );
 };
-// Memento class
-class TodoMemento {
-    constructor(state) {
-      this.state = state;
-    }
-    getState() {
-      return this.state;
-    }
-  }
-// Caretaker class
-class TodoCaretaker {
-    constructor() {
-      this.mementos = [];
-      this.currentIndex = -1;
-    }
-    getMemento(){
-        return this.mementos[this.mementos.length - 1]
-    }
-    addMemento(memento) {
-      this.mementos.push(memento);
-      this.currentIndex = this.mementos.length - 1;
-    }
-    undo() {
-      if (this.currentIndex > 0) {
-        this.currentIndex--;
-        return this.mementos[this.currentIndex];
-      }
-      return null;
-    }
-    redo() {
-      if (this.currentIndex < this.mementos.length - 1) {
-        this.currentIndex++;
-        return this.mementos[this.currentIndex];
-      }
-      return null;
-    }
-  }
 export default TodoList;
